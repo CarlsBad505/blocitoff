17e0ce3aa11205e14a1825181ae66f27e39672b1
carlsbad505 <carl@tabrific.com>
2015-11-02 18:01:01 +0000

    modify nav bar and title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def title_for(page_title)
-    page_title.empty? ? "BlocItOff" : page_title
+    page_title.empty? ? "BlocItOff" : "BlocItOff | " + page_title
   end
 end

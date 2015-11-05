e94f549afc1b722b64b088b061e87df4a4e01620
carlsbad505 <carl@tabrific.com>
2015-11-05 21:38:04 +0000

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,23 @@
-20.times do
-  Faker::Name.name
-end
-# Check seed file for bloccit for reference
 
+# Create Users
+20.times do
+  user = User.create!(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Internet.password(minimum_length=6)
+    )
+end
+
+users = User.all
+
+# Create Items
+100.times do
+  item = Item.create!(
+    name: Faker::Lorem.sentence(word_count=4),
+    user: users.sample
+    )
+end
+
+puts "Seed Finished"
+puts "#{User.count} users created"
+puts "#{Item.count} items created"

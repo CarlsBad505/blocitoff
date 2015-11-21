2d51e410b7da01f2f2360aa5772d8093e7d60b95
carlsbad505 <carl@tabrific.com>
2015-11-21 02:07:28 +0000

    fix nav bar, add styling, integrate tracking API

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,14 @@
-var blocmetrics = {};
-  blocmetrics.report = function(items){
-    var event = {event: { name: items}};
-    var request = new XMLHttpRequest();
-    request.open("POST", "https://bloc-foundation-carlsbad505.c9.io:8081/api/events", true);
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send(JSON.stringify(event));
-  };
-  
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
 //= require_tree .
+
+var blocmetrics = {};
+  blocmetrics.report = function(event){
+    var event = {event: { name: event}};
+    var request = new XMLHttpRequest();
+    request.open("POST", "https://bloc-foundation-carlsbad505.c9.io:8081/api/events", true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(event));
+  };
